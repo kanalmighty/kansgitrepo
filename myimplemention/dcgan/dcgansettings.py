993caeaaa70f6b152c7cec66c920019da6b69806
kanalmighty <232701147@qq.com>
2019-09-27 20:02:38 +0800

    second commit

--- myimplemention/dcgan/dcgansettings.py
+++ myimplemention/dcgan/dcgansettings.py
@@ -1,20 +1,3 @@
-TEST_IMAGE_ROOT_PATH = 'D:\\pycharmspace\\testimage\\'
-TEST_RAW_IMAGE_PATH = TEST_IMAGE_ROOT_PATH + 'raw\\'
-TEST_GEOMETRY_IMAGE_PATH = TEST_IMAGE_ROOT_PATH + 'rotated\\'
-
-PASSWORD = '123456'
-
-POOL_LOWER_THRESHOLD = 10
-POOL_UPPER_THRESHOLD = 100
-
-CROPED_WIDTH = 511
-CROPED_HEIGHT = 383
-
-VALID_CHECK_CYCLE = 60
-POOL_LEN_CHECK_CYCLE = 20
-
-TEST_API = 'https://www.baidu.com'
-
 # Root directory for dataset
 # DCGAN_IMAGE_ROOT = '/content/sample_data'
 DCGAN_IMAGE_ROOT = 'D:\\pycharmspace\\cifar\\'
