6cf6832ca4e77b9027ee140302abfb85c820d3e6
kanalmighty <232701147@qq.com>
2019-10-21 13:33:20 +0800

    second commit

--- highperformance/simu.py
+++ highperformance/simu.py
@@ -88,6 +88,11 @@ def benchmark():
     simulator = ParticleSimulator(particles)
     simulator.evolve(0.1)
 
+def benchmark_memory():
+    particles = [Particles(uniform(-1.0, 1.0), uniform(-1.0, 1.0), uniform(-1.0, 1.0)) for i in range(1000)]
+    simulator = ParticleSimulator(particles)
+    simulator.evolve(0.1)
+
 
 if __name__ == '__main__':
     benchmark()
