af7d0f046c38d4b2cb4cf7142bf204ac25cfbe60
kanalmighty <232701147@qq.com>
2019-10-26 14:42:54 +0800

    second commit

--- isic1/models.py
+++ isic1/models.py
@@ -7,9 +7,10 @@ from data.datasets import ISICDataset
 from torch.utils.data import DataLoader
 model = torchvision.models.resnet18(pretrained=True)
 options = BaseOptions()
+
 args = options.get_args()
 transforms = utils.get_trainsforms(args)
-
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 isic = ISICDataset(args, transforms)
 ld = DataLoader(isic, batch_size=2, shuffle=True,)
@@ -18,6 +19,7 @@ criteria = nn.CrossEntropyLoss()
 loss_array = []
 for EPOCH in range(1):
     for x, y in ld:
+        x = x.to(device)
         y_hat = model(x.view(2, 3, 767, 1022).float())
         loss = criteria(y_hat, y.long())
         print(loss)
