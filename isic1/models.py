3a554cb05882f00e2fc29ea9c0d21505242990dc
kanalmighty <232701147@qq.com>
2019-10-26 15:42:58 +0800

    second commit

--- isic1/models.py
+++ isic1/models.py
@@ -14,14 +14,14 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 isic = ISICDataset(args, transforms)
 ld = DataLoader(isic, batch_size=2, shuffle=True,)
-optimizer = torch.optim.Adam(model.parameters(),0.003)
+optimizer = torch.optim.Adam(model.parameters(), 0.003)
 criteria = nn.CrossEntropyLoss()
 loss_array = []
 for EPOCH in range(1):
     for x, y in ld:
         x = x.to(device)
         y_hat = model(x.view(2, 3, 767, 1022).float())
-        loss = criteria(y_hat, y.long())
+        loss = criteria(y_hat, torch.cuda.LongTensor(y))
         print(loss)
         optimizer.zero_grad()
         loss.backward()
