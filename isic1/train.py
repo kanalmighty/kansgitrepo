71dea5d765786b4ed196bc8e9f196317a0bf0d12
kanalmighty <232701147@qq.com>
2019-11-06 13:05:21 +0800

    dd

--- isic1/train.py
+++ isic1/train.py
@@ -35,23 +35,22 @@ loss_dict_draw = {}
 
 model.train()
 for EPOCH in range(args.epoch):
-    loss_total_per_epoch = 0#记录每个epoch,所有batch的loss总和
+    loss_avg_per_epoch = 0#记录每个epoch,所有batch的loss总和
     for idx, (x, y) in enumerate(trainingdata_loader):
         loss_dict_print = {}
         x = x.to(device)
         y = torch.argmax(y, dim=1)
         y_hat = model.network(x.float())
         loss = criteria(y_hat, y.long().to(device))
-        loss_total_per_epoch += loss.item()#获取所有batch的loss总和
+        loss_avg_per_epoch += loss.item()#获取所有batch的loss总和
         # 传入的data是一给字典，第个位置是epoch,后面是损失函数名:值
         loss_dict_print['EPOCH'] = EPOCH
-        loss_dict_print['cross_loss'] = loss.item()
+        loss_dict_print['cross_loss'] = loss_avg_per_epoch
         # loss_dict_print，没有epoch,都是损失函数名:值（值是list）
         visualizer.get_data_report(loss_dict_print)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    loss_avg_per_epoch = loss_total_per_epoch/(args.batchsize*idx)#获取这个epoch中一个平input的均loss
     loss_list_draw.append(loss_avg_per_epoch)
 loss_dict_draw['cross_loss'] = loss_list_draw
 logger.set_arguments(vars(args))
