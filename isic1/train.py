091d8783c338ca5ac27d8b6ff2dae1ad3b2ff5ce
kanalmighty <232701147@qq.com>
2019-11-26 19:11:27 +0800

    dd

--- isic1/train.py
+++ isic1/train.py
@@ -31,39 +31,41 @@ label_path = configer['trainingLabelPath']
 isic = ISICDataset(image_path, label_path, transforms)
 isic.__assert_equality__()
 trainingdata_loader = DataLoader(isic, batch_size=args.batchsize, shuffle=True, drop_last=True)
+
 optimizer = model.optimizer
 criteria = model.loss_function
 logger.set_arguments(vars(args))
-loss_list_draw = []
-loss_dict_draw = {}
-train_accuracy_dict = {}
+epoch_loss_list = []
+epoch_statics_dict = {}#record epochly training statics
+train_statics_dict = {}#record overall training statics
 model.train()
 for EPOCH in range(args.epoch):
     loss_all_samples_per_epoch = 0#记录每个epoch,所有batch的loss总和
-    train_accuracy = 0
+    train_accuracy = 0#trainnig accuaracy per epoch
     for idx, (x, y) in enumerate(trainingdata_loader):
-        loss_dict_print = {}
+        batch_statics_dict = {}
         x = x.to(device)
         y = torch.argmax(y, dim=1)
         y_hat = model.network(x.float())
-        train_accuracy += (y.to(device) == torch.argmax(y_hat, dim=1)).sum()
+        train_accuracy += (y.to(device) == torch.argmax(y_hat, dim=1)).sum().item()
         loss = criteria(y_hat, y.long().to(device))
         loss_all_samples_per_epoch += loss.item()#loss.item()获取的是每个batchsize的平均loss
         # 传入的data是一给字典，第个位置是epoch,后面是损失函数名:值
-        loss_dict_print['EPOCH'] = EPOCH
-        loss_dict_print[args.lossfunction] = loss.item()
-        #get accuracy for each epoch
-        loss_dict_print['TRAIN ACCURACY'] = train_accuracy.item()/((idx+1)*args.batchsize)
-        # loss_dict_print，没有epoch,都是损失函数名:值（值是list）
-        visualizer.get_data_report(loss_dict_print)
+        batch_statics_dict['EPOCH'] = EPOCH
+        batch_statics_dict[args.lossfunction] = loss.item()
+        # loss_dict_print，每个epoch,都是损失函数名:值（值是list）
+        visualizer.get_data_report(batch_statics_dict)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
     loss_avg_per_epoch = loss_all_samples_per_epoch/(idx+1)#获取这个epoch中一个平input的均loss,idx从0开始，所以需要加1
-    loss_list_draw.append(loss_avg_per_epoch)
-loss_dict_draw[args.lossfunction] = loss_list_draw
-loss_dict_draw['TRAIN ACCURACY'] = train_accuracy/((idx+1)*args.batchsize)
-logger.set_training_data(loss_dict_draw)
+    train_accuracy_epoch = train_accuracy / len(isic)#training accuracy/sample numbers
+    epoch_statics_dict['AVG LOSS'] = loss_avg_per_epoch
+    epoch_loss_list.append(loss_avg_per_epoch)#record epoch loss for drawing
+    epoch_statics_dict['TRAINING ACCURACY'] = train_accuracy_epoch
+    visualizer.get_data_report(epoch_statics_dict)
+    train_statics_dict[EPOCH] = epoch_statics_dict
+logger.set_training_data(train_statics_dict)
 logger.write_training_data()
-visualizer.draw_picture_block(loss_dict_draw)
+visualizer.draw_picture_block(epoch_loss_list)
 pkl_name = model.save_model(logger.date_string, logger.start_time_string)
