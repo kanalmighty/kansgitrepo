57969afe4f5c64a27fc6d2c5de6787fe8e5d4b47
kanalmighty <232701147@qq.com>
2019-12-15 17:22:33 +0800

    dd

--- isic1/utils.py
+++ isic1/utils.py
@@ -238,9 +238,8 @@ def get_expand_border(w, h, target_size):
 
 #input a path,search for csv and return file name
 def get_csv_by_path_name(path):
-    csv = glob.glob(os.path.join(path, '*.csv'))
-    print(path, csv)
-    return csv[0]
+    csv_path = glob.glob(os.path.join(path, '*.csv'))
+    return csv_path
 # def encode_image_name(file_list, index=0):
 #     file_list_encoded = []
 #     if not isinstance(file_list, list):
