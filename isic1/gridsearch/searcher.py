dc9bfd4f5ec81851fe046354ff0107a6191c8e9a
kanalmighty <232701147@qq.com>
2019-12-29 15:16:21 +0800

    dd

--- isic1/gridsearch/searcher.py
+++ isic1/gridsearch/searcher.py
@@ -68,8 +68,6 @@ class Searcher:
             if EPOCH > 1:
                 loss_descend_rate = epoch_statics_list[-1]['AVG LOSS']/epoch_statics_list[-2]['AVG LOSS']
                 if loss_descend_rate >= self.setting.lossDescendThreshold:
-                    print(epoch_statics_list[-1]['AVG LOSS'])
-                    print(epoch_statics_list[-2]['AVG LOSS'])
                     print('current loss descend rate is %f ,larger than threshold %f, abandon this SPD' % (loss_descend_rate, self.setting.lossDescendThreshold))
                     self.is_abandoned = 1
                     break
