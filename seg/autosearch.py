86f0f489b3c9188b8454eb97a57fbab29a126955
kanalmighty <232701147@qq.com>
2020-04-13 19:04:36 +0800

    dd

--- seg/autosearch.py
+++ seg/autosearch.py
@@ -131,9 +131,10 @@ def train(args):
                     cv2.imwrite(mask_root + str(idx) + '.jpg', test_image_contour)
 
             test_accuracy_list.append(test_accuracy_count_epoch / (total_test_length))
-        pdb.set_trace()
-        is_promising = True
+
         # 第二个epoch开始计算acc增长率
+        is_promising = True
+
         if EPOCH >= 1:
             is_promising = utils.check_acc_rate(test_accuracy_list, args.testAccThreshold, args.epoch - EPOCH)
         if is_promising is not True:
