2b5f172fd6233f2130a7c91c1311a584452b5ca0
kanalmighty <232701147@qq.com>
2020-06-11 21:06:33 +0800

    d

--- pytorch-YOLOv4/netvisual.py
+++
@@ -1,7 +0,0 @@
-import torch
-from tensorboardX import SummaryWriter
-from models import *
-dummy_input = torch.rand(8, 3, 320, 320) #假设输入13张1*28*28的图片
-model = Yolov4()
-with SummaryWriter(comment='aa') as w:
-    w.add_graph(model, (dummy_input, ))
--- pytorch-YOLOv4/test.py
+++
@@ -1,8 +0,0 @@
-import torch
-import pycuda.gpuarray as gpuarray
-import numpy as np
-
-x = torch.tensor([1,1,1,1])
-y = torch.tensor([[2,2,2,2],[2,2,2,2]])
-z = torch.tensor([[[2,2,2,2]],[[3,3,3,3]],[[4,4,4,4]]])
-print(z+y)
